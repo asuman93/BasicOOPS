31583518999d7cea531250f8414cbc111d2b0ed5
asuman93 <abhishek.suman93@gmail.com>
2025-06-08 17:39:47 +0530

    First

---
+++ demo/src/main/java/com/example/demo/BasicOopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicOopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicOopsApplication.class, args);
+	}
+
+}
